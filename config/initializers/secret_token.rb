6001bd3451748f0a213f6eb061afb672e406c13d
Eugen M <eugen_mll@arcor.de>
2013-01-29 20:05:20 +0100

    Initial commit lumenpix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lumenpix::Application.config.secret_token = '83c7821eadbe6746794d89eb52501d5b6c8c7e205707f9e888842fd07400bd1b0043d383de6a05ed5268b4b8d8a946c41ac835818fa16f6ecd2a28a1c3091573'
